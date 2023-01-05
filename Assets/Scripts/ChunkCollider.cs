9eefb971495d6612d03bd5abad22d31b4baf21a6
Nikolay <grozdovnikolay@gmail.com>
2023-01-05 17:44:29 +0300

    Chunk collider optimization.

--- Assets/Scripts/ChunkCollider.cs
+++ Assets/Scripts/ChunkCollider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,9 +10,9 @@ namespace Minecraft {
         [SerializeField]
         private MeshCollider meshCollider;
 
-        private Mesh mesh;
-
         public void UpdateMesh(ConcurrentDictionary<MaterialType, MeshData> meshData) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();  
             List<Vector3> vertices = new();
             List<ushort> indices = new();
             foreach (var pair in meshData) {
@@ -22,17 +23,18 @@ namespace Minecraft {
                 vertices.AddRange(pair.Value.ColliderVertices);
             }
 
+            var mesh = new Mesh();
+            mesh.Clear();
             mesh.SetVertexBufferParams(vertices.Count,
                 new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3));
             mesh.SetVertexBufferData(vertices, 0, 0, vertices.Count);
             mesh.SetIndexBufferParams(indices.Count, IndexFormat.UInt16);
             mesh.SetIndexBufferData(indices, 0, 0, indices.Count);
             mesh.SetSubMesh(0, new SubMeshDescriptor(0, indices.Count));
+            mesh.UploadMeshData(true);
             meshCollider.sharedMesh = mesh;
-        }
-
-        private void Awake() {
-            mesh = new();
+            stopwatch.Stop();
+            UnityEngine.Debug.Log(stopwatch.ElapsedTicks);
         }
     }
 }
